8c88b48e894325d98af5a274ecc32cadaf807eca
Ian Tolentino <165245557+iantolentino@users.noreply.github.com>
2026-03-29 13:51:23 +0800

    Delete api/get_session.py

--- api/get_session.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-api_id = 123456 # Your API ID
-api_hash = 'your_hash'
-
-with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print("YOUR_NEW_SESSION_STRING:")
-    print(client.session.save())
